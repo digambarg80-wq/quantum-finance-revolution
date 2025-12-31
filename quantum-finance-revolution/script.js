2a48d1d2f48f176e926f2f4149fd3f3c73b06800
Digambar Raju Gaikwad <digambarg80@gmail.com>
2025-12-31 18:55:39 +0530

    Add files via upload

---
+++ quantum-finance-revolution/script.js
@@ -0,0 +1,5 @@
+document.getElementById("downloadBtn").addEventListener("click", (here) => {
+  alert("Whitepaper download will be available soon!");
+  console.log("Welcome to my page here")
+});
+console.log("Welcome to my page here")
